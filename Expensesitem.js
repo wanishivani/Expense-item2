b6085a7ebaf9641283d23c9312227a70ce7fe449
wanishivani <127400106+wanishivani@users.noreply.github.com>
2023-04-19 14:18:25 +0530

    Add files via upload

---
+++ Expensesitem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return(<div>
+        <h1>ExpenseItem!</h1>
+      <h2>Food-Rs 10</h2>
+    <h2>Petrol Rs 100</h2>
+   <h2> Movie Rs 200</h2>
+    </div>)
+    
+    
+    
+    
+}
+export default ExpenseItem;
